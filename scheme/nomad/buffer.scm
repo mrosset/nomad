dfde51289cbf5abf3dc79d4e39cbc3b6599c79a1
mike rosset <mike.rosset@gmail.com>
2018-05-19 10:36:52 -0700

    broswer.scm and webkit.scm are SOURCES

--- scheme/nomad/buffer.scm
+++ scheme/nomad/buffer.scm
@@ -17,8 +17,6 @@
 ;; with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (nomad buffer)
-  #:use-module (nomad app)
-  #:use-module (nomad buffer-internal)
   #:use-module (srfi srfi-9)
   #:export (buffer-with-id pp-buffers))
 
