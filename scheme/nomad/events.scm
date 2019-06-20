06d73506a729a70885b74d2c52f1d4e3fa367a79
Amar Singh <nly@disroot.org>
2019-06-20 18:31:34 +0530

    cleanup (nomad events)

--- scheme/nomad/events.scm
+++ scheme/nomad/events.scm
@@ -18,7 +18,8 @@
 
 (define-module (nomad events)
   #:use-module (ice-9 threads)
-  #:export (event-hook debug-event))
+  #:export (event-hook
+            debug-event))
 
 (define event-hook (make-hook 1))
 
