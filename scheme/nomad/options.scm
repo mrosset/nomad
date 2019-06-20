70b4cde8ad4bc07880baaa46bafcac1114238325
Amar Singh <nly@disroot.org>
2019-06-21 02:59:00 +0530

    add license header to options.scm

--- scheme/nomad/options.scm
+++ scheme/nomad/options.scm
@@ -1,12 +1,28 @@
+;; Options.scm --- command line options for nomad
+;; Copyright (C) 2019 Michael Rosset <mike.rosset@gmail.com>
+
+;; This file is part of Nomad
+
+;; Nomad is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Nomad is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;; See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (nomad options)
   #:use-module (nomad browser)
   #:use-module (ice-9 getopt-long)
-  #:export (
-	    display-options
+  #:export (display-options
 	    option-client
 	    option-listen
-	    option-url
-	    ))
+	    option-url))
 
 (define option-spec
   '((listen  (value #t))
