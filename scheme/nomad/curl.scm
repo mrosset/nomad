c2896045e28263100a70fef350e2305ddf43814c
Amar Singh <nly@disroot.org>
2019-07-15 21:31:38 +0530

    downloader: add module (nomad curl)
    something in here broke guix-local.scm builds?

---
+++ scheme/nomad/curl.scm
@@ -0,0 +1,27 @@
+;; Curl --- download things from network protocols
+
+;; Copyright (C) 2019 Amar Singh<nly@disroot.org>
+
+;; This file is part of Nomad.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+(define-module (nomad curl)
+  #:use-module (curl)
+  #:export (curl-download))
+
+(define* (curl-download url)
+  (let ((handle (curl-easy-init)))
+    (curl-easy-setopt handle 'url url)
+    (curl-easy-perform handle #t)))
