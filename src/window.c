f6b4137701cb0497cbf791eb6ecdcee42a222e75
Mike Rosset <mrosset@bufio.org>
2019-07-23 23:11:07 -0700

    Revert "gtk: nomad_app_window_get_webview no longer uses NomadBuffer class"
    This reverts commit 84a795108c80cc15053c1533b815f3ce30467720.

--- src/window.c
+++ src/window.c
@@ -634,9 +634,12 @@ nomad_app_window_new (NomadApp *app)
 WebKitWebView *
 nomad_app_window_get_webview (NomadAppWindow *self)
 {
-  GtkNotebook *notebook = nomad_window_get_notebook (self);
-  int page = gtk_notebook_get_current_page (notebook);
-  return WEBKIT_WEB_VIEW (gtk_notebook_get_nth_page (notebook, page));
+  NomadBuffer *buf = nomad_app_window_get_buffer (self);
+  if (!buf)
+    {
+      return NULL;
+    }
+  return nomad_buffer_get_view (buf);
 }
 
 SCM_DEFINE (scm_nomad_window_focus, "webview-focus", 0, 0, 0, (),
