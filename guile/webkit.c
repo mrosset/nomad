171bbdc4299a176df407e22aad52fd960c390c64
Amar Singh <nly@disroot.org>
2019-07-28 17:51:16 +0530

    webkit.c: proxy: formatting

--- guile/webkit.c
+++ guile/webkit.c
@@ -87,29 +87,23 @@ SCM_DEFINE_PUBLIC (scm_nomad_webkit_network_proxy_settings_new,
 SCM_DEFINE_PUBLIC (scm_nomad_webkit_network_proxy_settings_free,
                    "webkit-proxy-settings-free", 1, 0, 0, (SCM pointer),
                    "Frees up a webkit proxy settings.")
-{
-  webkit_network_proxy_settings_free (scm_to_pointer (pointer));
-  return SCM_UNSPECIFIED;
-}
+{webkit_network_proxy_settings_free (scm_to_pointer (pointer));
+  return SCM_UNSPECIFIED;}
 
 SCM_DEFINE_PUBLIC (scm_nomad_webkit_network_proxy_settings_copy,
                    "webkit-proxy-settings-copy", 1, 0, 0, (SCM pointer),
                    "Copies a webkit proxy settings.")
-{
-  return scm_from_pointer (webkit_network_proxy_settings_copy
-                           (scm_to_pointer (pointer)), NULL);
-}
+{return scm_from_pointer (webkit_network_proxy_settings_copy
+                           (scm_to_pointer (pointer)), NULL);}
 
 SCM_DEFINE_PUBLIC (scm_nomad_webkit_network_proxy_add_proxy_for_scheme,
                    "webkit-proxy-settings-add-proxy-for-scheme"
                    , 3, 0, 0, (SCM pointer, SCM scheme, SCM proxy),
                    "Adds a URI-scheme-specific proxy. URIs whose scheme matches scheme will be proxied via proxy.")
-{
-  webkit_network_proxy_settings_add_proxy_for_scheme
+{webkit_network_proxy_settings_add_proxy_for_scheme
     (scm_to_pointer (pointer), scm_to_locale_string (scheme),
      scm_to_locale_string (scheme));
-  return SCM_UNSPECIFIED;
-}
+  return SCM_UNSPECIFIED;}
 
 SCM_DEFINE_PUBLIC (scm_nomad_webkit_web_context_set_network_proxy_settings_custom,
                    "webkit-set-proxy-settings-custom", 1, 0, 0,
@@ -123,9 +117,8 @@ SCM_DEFINE_PUBLIC (scm_nomad_webkit_web_context_set_network_proxy_settings_custo
 SCM_DEFINE_PUBLIC (scm_nomad_webkit_web_context_set_network_proxy_settings_default,
                    "webkit-set-proxy-settings-default", 0, 0, 0,
                    (), "Activate system default proxy.")
-{
-  webkit_web_context_set_network_proxy_settings (webkit_web_context_get_default(),
-                                                 WEBKIT_NETWORK_PROXY_MODE_DEFAULT, NULL);
+{webkit_web_context_set_network_proxy_settings (webkit_web_context_get_default(),
+                                                WEBKIT_NETWORK_PROXY_MODE_DEFAULT, NULL);
   return SCM_UNSPECIFIED;}
 
 SCM_DEFINE_PUBLIC (scm_nomad_webkit_web_context_set_network_proxy_settings_no_proxy,
