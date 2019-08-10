bc93c3bcd3d67d17ad2d5a0e806c3fc560c16d9f
Amar Singh <nly@disroot.org>
2019-08-10 09:33:56 +0530

    guix: Add guile-g-golf.
    *  (guile-g-golf): New variable.
    
    Produces error: In procedure dynamic-link: file: "libgirepository-1.0",
    message: "file not found"
    
    Commit ff111f6 from master branch.

--- guix/gnu/packages/nomad.scm
+++ guix/gnu/packages/nomad.scm
@@ -112,10 +112,134 @@
                            prefix ,go-path))
                     progs)
                #t))))))
-    (home-page "https://savannah.nongnu.org/projects/nomad/")
-    (synopsis "Extensible Web Browser in Guile Scheme")
-    (description "Nomad is an Emacs-like Web Browser built using Webkitgtk and
-Emacsy.  It has a small C layer and most browser features are fully
-programmable in Guile.  It has hooks, keymaps, and self documentation
-features.")
-    (license license:gpl3+)))
+    (inputs
+     `(("guile" ,guile-2.2)
+       ("gnupg" ,gnupg)
+       ("xclip" ,xclip)))))
+
+(define-public guile-g-golf
+  (let ((commit "ff111f699d6e544750140dc0e41c1bfc66d00049"))
+    (package
+      (name "guile-g-golf")
+      (version (git-version "1" "1" commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://git.savannah.gnu.org/git/g-golf.git")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "0m8hv85sd2mc7s04b6hmq7wxl2x2nc65nl4i38rcs2g5isfk2rcm"))))
+      (build-system glib-or-gtk-build-system)
+      (native-inputs
+       `(("autoconf" ,autoconf)
+         ("automake" ,automake)
+         ("texinfo" ,texinfo)
+         ("gettext" ,gettext-minimal)
+         ("libtool" ,libtool)
+         ("pkg-config" ,pkg-config)))
+      (inputs
+       `(("guile" ,guile-2.2)
+         ("guile-lib" ,guile-lib)
+         ("glib" ,glib)
+         ("gobject-introspection" ,gobject-introspection)))
+      (arguments
+       `(;; #:configure-flags '("--with-guile-site=yes")
+         ))
+      (home-page "https://www.gnu.org/software/g-golf/")
+      (synopsis "G-Golf is a Guile Object Library for GNOME.")
+      (description "G-Golf low level API comprises a binding to - (most of) the
+GObject Introspection and (some of) the GObject and Glib libraries, as well as
+additional (G-Golf) utilities - used to import GObject libraries and build
+their corresponding G-Golf high level API.")
+      (license license:lgpl3+))))
+
+(define-public nomad
+  (let ((commit "1a1bb61048cfc34b83d388e38b5612d369a2b9df"))
+    (package
+      (name "nomad")
+      (version (git-version "0.0.4-alpha" "375" commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://git.savannah.gnu.org/git/nomad.git")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1a4am9ak98y0p3ibiji14lr99lqplis08lka2kz0zyqalh4y8c68"))))
+      (build-system gnu-build-system)
+      (native-inputs
+       `(("autoconf" ,autoconf)
+         ("automake" ,automake)
+         ("pkg-config" ,pkg-config)
+         ("libtool" ,libtool)
+         ("guile" ,guile-2.2)
+         ("glib:bin" ,glib "bin")))
+      (inputs
+       `(("guile" ,guile-2.2)
+         ("guile-lib" ,guile-lib)
+         ("guile-gcrypt" ,guile-gcrypt)
+         ("guile-readline" ,guile-readline)
+         ("gnutls" ,gnutls)
+         ("shroud" ,shroud-0.1.2)
+         ;; waiting on shroud to be updated in guix
+         ("emacsy" ,emacsy-git)
+         ;; emacsy needs to be updated in guix
+         ("glib" ,glib)
+         ("dbus-glib" ,dbus-glib)
+         ("gtk+" ,gtk+)
+         ("gtksourceview" ,gtksourceview)
+         ("webkitgtk" ,webkitgtk)
+         ("xorg-server" ,xorg-server)))
+      (propagated-inputs
+       `(("glib-networking" ,glib-networking)
+         ("gsettings-desktop-schemas"
+          ,gsettings-desktop-schemas)))
+      (arguments
+       `(#:tests? #t
+         #:modules ((guix build gnu-build-system)
+                    (guix build utils)
+                    (ice-9 popen)
+                    (ice-9 rdelim)
+                    (srfi srfi-26))
+         #:phases
+         (modify-phases %standard-phases
+           (add-before 'check 'start-xorg-server
+                     (lambda* (#:key inputs #:allow-other-keys)
+                       ;; The test suite requires a running X server.
+                       (system (format #f "~a/bin/Xvfb :1 &"
+                                       (assoc-ref inputs "xorg-server")))
+                       (setenv "DISPLAY" ":1")
+                       #t))
+           (add-after 'install 'wrap-binaries
+             (lambda* (#:key inputs outputs #:allow-other-keys)
+               (let* ((out (assoc-ref outputs "out"))
+                      (gio-deps (map (cut assoc-ref inputs <>) '("glib-networking" "glib")))
+                      (gio-mod-path (map (cut string-append <> "/lib/gio/modules") gio-deps))
+                      (effective (read-line (open-pipe* OPEN_READ
+                                                        "guile" "-c"
+                                                        "(display (effective-version))")))
+                      (deps (map (cut assoc-ref inputs <>) '("emacsy" "guile-lib"
+                                                             "guile-readline" "shroud")))
+                      (scm-path (map (cut string-append <>
+                                          "/share/guile/site/" effective)
+                                     `(,out ,@deps)))
+                      (go-path (map (cut string-append <>
+                                         "/lib/guile/" effective "/site-ccache")
+                                    `(,out ,@deps)))
+                      (progs (map (cut string-append out "/bin/" <>)
+                                  '("nomad"))))
+                 (map (cut wrap-program <>
+                           `("GIO_EXTRA_MODULES" ":" prefix ,gio-mod-path)
+                           `("GUILE_LOAD_PATH" ":" prefix ,scm-path)
+                           `("GUILE_LOAD_COMPILED_PATH" ":"
+                             prefix ,go-path))
+                      progs)
+                 #t))))))
+      (home-page "https://savannah.nongnu.org/projects/nomad/")
+      (synopsis "Web Browser extensible in Guile scheme")
+      (description "Nomad is a Emacs-like web browser that consists of a small
+C backend and modular feature-set fully programmable in Guile.")
+      (license license:gpl3+))))
