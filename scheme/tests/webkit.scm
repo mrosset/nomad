27656aa9278645f7ca2233833f7855f6024ada48
Mike Rosset <mike.rosset@gmail.com>
2019-08-14 04:54:27 -0700

    tests: g-golf tests for frame and webkit

--- scheme/tests/webkit.scm
+++ scheme/tests/webkit.scm
@@ -17,27 +17,36 @@
 ;; with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (test webkit)
-  #:use-module (nomad app)
-  #:use-module (nomad webkit)
-  #:use-module (system foreign)
-  #:use-module (emacsy emacsy)
-  #:use-module (oop goops)
   #:use-module (g-golf)
   #:use-module (srfi srfi-64))
 
+(gi-import "Gtk")
+(gi-import "WebKit2")
+
 (test-skip "webkit")
 
+(let ((gtk? (gtk-init-check #f #f)))
+  (test-assert "Gtk init?" gtk?))
+
 (test-group "webkit"
-            (let* ((buffer (make <buffer>))
-                   (view (webkit-new)))
-              (test-assert "webview is pointer?"
-                (pointer? view))
-              (test-assert "view is not null"
-                (not (null-pointer? view)))
-              (test-equal "no uri"
-                "NULL"
-                (webkit-uri view))
-              (test-equal "load uri"
-                "https://gnu.org/"
-                (begin (webkit-load-uri view "https://gnu.org")
-                       (webkit-uri view)))))
+            (let* ((settings (webkit-network-proxy-settings-new "localhost:8080"
+                                                                '("*.gnu.org")))
+                   (view (make <web-kit-web-view>))
+                   (view-context (webkit-web-view-get-context view))
+                   (global-context (webkit-web-context-get-default))
+                   (new-context (make <web-kit-web-context>)))
+
+              (test-assert (not (null? proxy-settings))) ;; Fails because its '()
+              (test-assert (not (unspecified? view-context))) ;; Fails because it's unspecified
+              (test-assert (not (unspecified? global-context))) ;; Fails because it's unspecified
+              (test-assert (not (unspecified? new-context)))    ;; Passes
+              (test-equal <web-kit-web-context> (class-of view-context)) ;; Fails
+              (test-equal <web-kit-web-context> global-context)          ;; Fails
+              (test-equal <web-kit-web-context> (class-of new-context))  ;; Passes
+
+              ;; setting the proxy setting fails with
+              ;; In procedure sizeof: Wrong type argument in position 1: ()
+              ;;
+              ;; empty struct seems to be the culprit here
+              (webkit-web-context-set-network-proxy-settings new-context 'custom settings)
+   ))
