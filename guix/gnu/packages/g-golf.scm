48c50163348d8f6199ce0893ac671258c3c24dd5
Mike Rosset <mike.rosset@gmail.com>
2019-12-29 10:45:28 -0800

    guix: use the latest g-golf package declaration from giqt

--- guix/gnu/packages/g-golf.scm
+++ guix/gnu/packages/g-golf.scm
@@ -1,118 +1,84 @@
 (define-module (gnu packages g-golf))
 
-(use-modules
- (guix packages)
- (guix git-download)
- (guix gexp)
- (guix download)
- (guix build-system gnu)
- (guix build-system glib-or-gtk)
- ((guix licenses) #:prefix license:)
- (guix utils)
- (gnu packages autotools)
- (gnu packages curl)
- (gnu packages gettext)
- (gnu packages glib)
- (gnu packages gnome)
- (gnu packages gnupg)
- (gnu packages gtk)
- (gnu packages guile-xyz)
- (gnu packages guile)
- (gnu packages password-utils)
- (gnu packages pkg-config)
- (gnu packages tls)
- (gnu packages texinfo)
- (gnu packages webkit)
- (gnu packages xdisorg)
- (gnu packages xorg)
-)
+(use-modules ((guix licenses) #:prefix license:)
+             (guix utils)
+             (gnu packages autotools)
+             (gnu packages gettext)
+             (gnu packages glib)
+             (gnu packages gnome)
+             (gnu packages guile)
+             (gnu packages guile-xyz)
+             (gnu packages pkg-config)
+             (gnu packages texinfo)
+             (guix build-system gnu)
+             (guix download)
+             (guix git-download)
+             (guix packages))
 
 (define-public g-golf
-  (let ((texinfo-6.6
-         (package (inherit texinfo)
-                  (name "texinfo")
-                  (version "6.6")
-                  (source (origin
-                            (method url-fetch)
-                            (uri (string-append "mirror://gnu/texinfo/texinfo-"
-                                                version ".tar.xz"))
-                            (sha256
-                             (base32
-                              "0rixv4c301djr0d0cnsxs8c1wjndi6bf9vi5axz6mwjkv80cmfcv"))))
-                  (native-inputs '()))))
-
-    (let ((commit "722b6d6ae9dcb70d584860e1331dfc0aa8a2ba12"))
-      (package
-        (name "g-golf")
-        (version (git-version "1" "1" commit))
-        (source (origin
-                  (method git-fetch)
-                  (uri (git-reference
-                        (url "https://git.savannah.gnu.org/git/g-golf.git")
-                        (commit commit)))
-                  (file-name (git-file-name name version))
-                  (sha256
-                   (base32
-                    "06lwgjq4a9bxsznjigqj5rb5h467ijyxgyxhva4bwmx4l84l5whh"))))
-        (build-system gnu-build-system)
-        (native-inputs
-         `(("autoconf" ,autoconf)
-           ("automake" ,automake)
-           ("texinfo" ,texinfo-6.6)
-           ("gettext" ,gettext-minimal)
-           ("libtool" ,libtool)
-           ("clutter" ,clutter) ;; probably want to remove this clutter?
-           ;; FIXME: clutter is only required for tests
-           ("pkg-config" ,pkg-config)))
-        (inputs
-         `(("guile" ,guile-2.2)
-           ("guile-lib" ,guile-lib)
-           ("glib" ,glib)
-           ("gobject-introspection" ,gobject-introspection)))
-        (arguments
-         `(#:tests? #t
-           #:modules ((ice-9 popen)
-                    (ice-9 rdelim)
-                    ,@%gnu-build-system-modules)
-           #:phases
-           (modify-phases %standard-phases
-             (add-before 'configure 'setenv
-               (lambda _
+  (let ((commit "d7bee600d7cf2cbfd0972d339f04fe4c74213848"))
+    (package
+      (name "g-golf")
+      (version (git-version "1" "574" commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://git.savannah.gnu.org/git/g-golf.git")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1c39iy1ckjwyiyrb9a9sll4yzp45322y9p0rb77ljns0lz3qsgll"))))
+      (build-system gnu-build-system)
+      (native-inputs
+       `(("autoconf" ,autoconf)
+         ("automake" ,automake)
+         ("texinfo" ,texinfo)
+         ("gettext" ,gettext-minimal)
+         ("libtool" ,libtool)
+         ("pkg-config" ,pkg-config)))
+      (inputs
+       `(("guile" ,guile-2.2)
+         ("guile-lib" ,guile-lib)
+         ("glib" ,glib)
+         ("clutter" ,clutter)))
+      (propagated-inputs
+       `(("gobject-introspection" ,gobject-introspection)))
+      (arguments
+       `(#:tests? #f
+         #:phases
+         (modify-phases %standard-phases
+           (add-before 'configure 'substitute-libs
+             (lambda* (#:key inputs outputs #:allow-other-keys)
+               (let* ((get (lambda (key lib)
+                             (string-append (assoc-ref inputs key) "/lib/" lib)))
+                      (libgi      (get "gobject-introspection" "libgirepository-1.0.so"))
+                      (libglib    (get "glib" "libglib-2.0.so"))
+                      (libgobject (get "glib" "libgobject-2.0.so")))
+                 (substitute* "configure"
+                   (("SITEDIR=\"\\$datadir/g-golf\"")
+                    "SITEDIR=\"$datadir/guile/site/$GUILE_EFFECTIVE_VERSION\"")
+                   (("SITECCACHEDIR=\"\\$libdir/g-golf/")
+                    "SITECCACHEDIR=\"$libdir/"))
+                 (substitute* "g-golf/init.scm"
+                   (("libgirepository-1.0") libgi)
+                   (("libglib-2.0") libglib)
+                   (("libgobject-2.0") libgobject)
+                   (("\\(dynamic-link \"libg-golf\"\\)")
+                    (format #f "~s"
+                            `(dynamic-link
+                              (format #f "~alibg-golf"
+                                      (if (getenv "GUILE_GGOLF_UNINSTALLED")
+                                          ""
+                                          ,(format #f "~a/lib/"
+                                                   (assoc-ref outputs "out"))))))))
                  (setenv "GUILE_AUTO_COMPILE" "0")
-                 #t))
-              (add-before 'build 'substitute-libs
-                (lambda* (#:key inputs #:allow-other-keys)
-                  (let* ((get (lambda* (x #:optional (path ""))
-                                (string-append (assoc-ref inputs x) "/lib/" path)))
-                         (gi (get "gobject-introspection" "libgirepository-1.0.so"))
-                         (glib (get "glib" "libglib-2.0.so"))
-                         (gobject (get "glib" "libgobject-2.0.so")))
-                    (substitute* "g-golf/init.scm"
-                                (("libgirepository-1.0") gi)
-                                (("libglib-2.0") glib)
-                                (("libgobject-2.0") gobject))
-                    #t)))
-              (add-after 'install 'install-guile-modules
-                (lambda* (#:key inputs outputs #:allow-other-keys)
-                  (let* ((out (assoc-ref outputs "out"))
-                         (guile (assoc-ref inputs "guile"))
-                         (effective (read-line (open-pipe* OPEN_READ
-                                                           "guile" "-c"
-                                                           "(display (effective-version))")))
-                         (go-path (string-append out "/lib/guile/" effective "/site-ccache"))
-                         (scm-path (string-append out "/share/guile/site/" effective)))
-                    (mkdir-p go-path)
-                    (mkdir-p scm-path)
-                    (copy-recursively (string-append out
-                                                     "/lib/g-golf/guile/" effective "/site-ccache")
-                                      go-path)
-                    (copy-recursively (string-append out
-                                                     "/share/g-golf")
-                                      scm-path)))))))
-        (home-page "https://www.gnu.org/software/g-golf/")
-        (synopsis "G-Golf is a Guile Object Library for GNOME")
-        (description "G-Golf low level API comprises a binding to - (most of) the
+                 (setenv "GUILE_GGOLF_UNINSTALLED" "1")
+                 #t))))))
+      (home-page "https://www.gnu.org/software/g-golf/")
+      (synopsis "G-Golf is a Guile Object Library for GNOME")
+      (description "G-Golf low level API comprises a binding to - (most of) the
 GObject Introspection and (some of) the GObject and Glib libraries, as well as
 additional (G-Golf) utilities - used to import GObject libraries and build
 their corresponding G-Golf high level API.")
-        (license license:lgpl3+)))))
+      (license license:lgpl3+))))
