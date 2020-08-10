63191612d56d7098f9bd1004ba75c2ee3a289976
Mike Rosset <mike.rosset@gmail.com>
2020-08-10 10:22:50 -0700

    guix: nomad-git declaration now uses 'g-golf-git as it's input
    This fixes an issue where nomad-git and g-golf version could be out of sync.
    
    guix environment -L ./guix nomad-git now uses the g-golf-git
    input. This ensures we can the pin the "g-golf" input to a certain g-golf
    commit. Useful if we are using bug fixes or features that are not quite in
    Guix' g-golf package.

--- guix/gnu/packages/nomad.scm
+++ guix/gnu/packages/nomad.scm
@@ -1,10 +1,25 @@
-(define-module (gnu packages nomad))
+(define-module (gnu packages nomad)
+  #:use-module (srfi srfi-1)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix download)
+  #:use-module (gnu packages guile-xyz))
 
-(use-modules
- (guix packages)
- (guix git-download)
- (guix download)
- (gnu packages guile-xyz))
+(define-public g-golf-git
+  (let ((commit   "4e9847582367e66ba2aae07507765855e0f9c3d6")
+        (revision "816")
+        (hash     (base32 "05pppka6bqswpymydf5wpphm5v0ijlv36apg6mqcwbs2vjf8dnqy")))
+     (package
+      (inherit g-golf)
+      (name "g-golf-git")
+      (version (git-version "0.1.0" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://git.savannah.gnu.org/git/g-golf.git")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256 hash))))))
 
 (define-public nomad-git
   (let ((commit "0.1.2-alpha-337-g8962d4d")
@@ -19,4 +34,6 @@
                       (url "https://git.savannah.gnu.org/git/nomad.git")
                       (commit commit)))
                 (file-name (git-file-name name version))
-                (sha256 hash))))))
+                (sha256 hash)))
+      (inputs (cons `("g-golf" ,g-golf-git)
+                    (alist-delete "g-golf" (package-inputs nomad)))))))
