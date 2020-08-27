b46470eb8a7f431b276514caa1456f784f0dea06
Mike Rosset <mike.rosset@gmail.com>
2020-08-26 17:50:29 -0700

    guix: add emacsy-git package declaration.
    This also updates nomad-git to use the emacsy-git.

--- guix/gnu/packages/nomad.scm
+++ guix/gnu/packages/nomad.scm
@@ -6,6 +6,21 @@
   #:use-module (gnu packages gettext)
   #:use-module (gnu packages guile-xyz))
 
+(define-public emacsy-git
+  (let ((commit "v0.4.1-30-g34032ff")
+        (hash   (base32 "18kblii8cmk999k159lpdqhc1hc48ppag43kqz2j5nssm5y6smqq")))
+    (package
+      (inherit emacsy-minimal)
+      (name "emacsy-git")
+      (version (string-drop commit 1))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://git.savannah.gnu.org/git/emacsy.git")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256 hash))))))
+
 (define-public g-golf-git
   (let ((commit   "84e894eb7945c3bcdf7f8d5135c1be3efa524c92")
         (revision "822")
@@ -38,5 +53,6 @@
                 (sha256 hash)))
       (native-inputs (cons `("gettext" ,gnu-gettext)
                            (package-native-inputs nomad)))
-      (inputs (cons `("g-golf" ,g-golf-git)
-                    (alist-delete "g-golf" (package-inputs nomad)))))))
+      (inputs (cons* `("g-golf" ,g-golf-git)
+                     `("emacsy" ,emacsy-git)
+                    (alist-delete "emacsy" (alist-delete "g-golf" (package-inputs nomad))))))))
